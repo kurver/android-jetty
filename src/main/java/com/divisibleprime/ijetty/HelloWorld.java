a3800dd92b7ee7a926bb899db53b70baca37685e
Kerry Burke <kurv@calculus.(none)>
2011-09-29 22:50:55 +1300

    Able to start camera app.

--- src/main/java/com/divisibleprime/ijetty/HelloWorld.java
+++ src/main/java/com/divisibleprime/ijetty/HelloWorld.java
@@ -8,7 +8,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.ContentUris;
+import android.util.Log;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
 
 /* ------------------------------------------------------------ */
 /** Hello Servlet 
@@ -17,17 +25,25 @@ import javax.servlet.http.HttpServletResponse;
 public class HelloWorld extends HttpServlet 
 {
     String proofOfLife = null;
-    
+    private ContentResolver resolver;
+    private Context androidContext;
+
     /* ------------------------------------------------------------ */
     public void init(ServletConfig config) throws ServletException
     {
     	super.init(config);
     	//to demonstrate it is possible
         Object o = config.getServletContext().getAttribute("org.mortbay.ijetty.contentResolver");
-        android.content.ContentResolver resolver = (android.content.ContentResolver)o;
-        android.content.Context androidContext = (android.content.Context)config.getServletContext().getAttribute("org.mortbay.ijetty.context");
+        resolver = (android.content.ContentResolver)o;
+        androidContext = (android.content.Context)config.getServletContext().getAttribute("org.mortbay.ijetty.context");
         proofOfLife = androidContext.getApplicationInfo().packageName;
     }
+    
+    public ContentResolver getContentResolver()
+    {
+        return resolver;
+    }
+
 
     /* ------------------------------------------------------------ */
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
@@ -38,10 +54,27 @@ public class HelloWorld extends HttpServlet
     /* ------------------------------------------------------------ */
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
+      //define the file-name to save photo taken by Camera activity
+        String fileName = "new-photo-name.jpg";
+        //create parameters for Intent with filename
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, fileName);
+        values.put(MediaStore.Images.Media.DESCRIPTION,"Image capture by camera");
+        //imageUri is the current activity attribute, define and save it for later usage (also in onSaveInstanceState)
+        Uri imageUri = getContentResolver().insert(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        //startActivityForResult(intent, 1);
+        androidContext.startActivity(intent);
+        
+        
         response.setContentType("text/html");
         ServletOutputStream out = response.getOutputStream();
         out.println("<html>");
-        out.println("<h1>Hello From Servlet Land!</h1>");
+        out.println("<h1>Hello From blah Land!</h1>");
         out.println("Brought to you by: "+proofOfLife);
         out.println("</html>");
         out.flush();
